acb6ba8da58d4927f7644175ff372ca04b54ba90
CharSiew Guy <ianlv145@gmail.com>
2022-03-29 22:21:49 +0800

    socdnasoicbsa

--- src/ReplicatedStorage/Client/Controllers/BreadGunController.lua
+++ src/ReplicatedStorage/Client/Controllers/BreadGunController.lua
@@ -1,7 +1,11 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ContextActionService = game:GetService("ContextActionService")
+local RunService = game:GetService("RunService")
+
 local Packages = ReplicatedStorage.Packages
 local Knit = require(Packages.Knit)
 local Janitor = require(Packages.Janitor)
+local Timer = require(Packages.Timer)
 local HumanoidAnimatorUtils = require(Packages.HumanoidAnimatorUtils)
 
 local BreadGunController = Knit.CreateController { Name = "BreadGunController" }
@@ -12,8 +16,31 @@ function BreadGunController:KnitStart()
         local hum = character:WaitForChild("Humanoid")
         local animator = HumanoidAnimatorUtils.getOrCreateAnimator(hum)
         local holdingGun = animator:LoadAnimation(ReplicatedStorage.Client.Assets.Animations.HoldingGun)
-        holdingGun.Looped = true
         holdingGun:Play()
+
+        local pointingGun = animator:LoadAnimation(ReplicatedStorage.Client.Assets.Animations.PointingGun)
+        local lastPointedGun = 0
+
+        local function handleAction(actionName, inputState)
+            if actionName == "Shoot" then
+                if inputState == Enum.UserInputState.Begin then
+                    if not pointingGun.IsPlaying then
+                        pointingGun:Play(0.1)
+                        lastPointedGun = math.huge
+                    end
+                elseif inputState == Enum.UserInputState.End then
+                    lastPointedGun = os.time()
+                end
+            end
+        end
+
+        ContextActionService:BindAction("Shoot", handleAction, true, Enum.UserInputType.MouseButton1)
+
+        RunService.Heartbeat:Connect(function()
+            if os.time() > lastPointedGun + 1 then
+                pointingGun:Stop(0.3)
+            end
+        end)
     end)
 end
 
