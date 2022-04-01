201c79189020deee2ac0ede58700563470fddc87
CharSiewGuy <74772016+CharSiewGuy@users.noreply.github.com>
2022-04-01 15:20:33 +0800

    sht

--- src/ReplicatedStorage/Client/Controllers/BreadGunController.lua
+++ src/ReplicatedStorage/Client/Controllers/BreadGunController.lua
@@ -5,28 +5,35 @@ local RunService = game:GetService("RunService")
 local Packages = ReplicatedStorage.Packages
 local Knit = require(Packages.Knit)
 local Janitor = require(Packages.Janitor)
+local Promise = require(Packages.Promise)
 local Timer = require(Packages.Timer)
 local HumanoidAnimatorUtils = require(Packages.HumanoidAnimatorUtils)
 
 local BreadGunController = Knit.CreateController { Name = "BreadGunController" }
 BreadGunController._janitor = Janitor.new()
 
+BreadGunController.Stats = {
+    ["FireRate"] = 0.15
+}
+
 function BreadGunController:KnitStart()
     Knit.Player.CharacterAdded:Connect(function(character)
         local hum = character:WaitForChild("Humanoid")
         local animator = HumanoidAnimatorUtils.getOrCreateAnimator(hum)
-        local holdingGun = animator:LoadAnimation(ReplicatedStorage.Client.Assets.Animations.HoldingGun)
+        local holdingGun = animator:LoadAnimation(ReplicatedStorage.Assets.Animations.HoldingGun)
         holdingGun:Play()
 
         --AIMING
 
         self._aimJanitor = Janitor.new()
+        self.isFiring = false
+        self.canFire = true
 
         local function getLookAngle()
             return workspace.CurrentCamera.CFrame.LookVector.Y * 1.1
         end
 
-        local pointingGun
+        local pointingGun = animator:LoadAnimation(ReplicatedStorage.Assets.Animations.PointingGun)
 
         local function updateAim()
             local lookAngle = getLookAngle()
@@ -34,7 +41,8 @@ function BreadGunController:KnitStart()
             pointingGun.TimePosition = aimTimePos
         end
 
-        pointingGun = animator:LoadAnimation(ReplicatedStorage.Client.Assets.Animations.PointingGun)
+        local shooting = animator:LoadAnimation(ReplicatedStorage.Assets.Animations.Shooting)
+        shooting.Priority = Enum.AnimationPriority.Action3
 
         local function handleAction(actionName, inputState)
             if actionName == "Shoot" then
@@ -51,6 +59,8 @@ function BreadGunController:KnitStart()
                     end)
 
                     self._janitor:Add(self._aimJanitor)
+                    
+                    self.isFiring = true                   
                 elseif inputState == Enum.UserInputState.End then
                     if hum.MoveDirection.Magnitude ~= 0 then
                         self._aimJanitor:Cleanup()
@@ -59,14 +69,42 @@ function BreadGunController:KnitStart()
                             self._aimJanitor:Cleanup()
                         end))
                     end
+
+                    --[[self._janitor:AddPromise(Promise.delay(shooting.Length)):andThen(function()
+                        pointingGun:AdjustWeight(1, 0)
+                    end)]]--
+                    self.isFiring = false
                 end
             end
         end
         
         ContextActionService:BindAction("Shoot", handleAction, true, Enum.UserInputType.MouseButton1)
 
+        self._janitor:Add(RunService.Heartbeat:Connect(function()
+            if self.canFire then
+                if self.isFiring then
+                    self.canFire = false
+
+                    shooting:Play(0)
+                    
+                    local flash = ReplicatedStorage.Assets.Particles.ElectricMuzzleFlash:Clone()
+                    flash.Parent = character.breadgun.Handle.Muzzle
+                    flash:Emit(1)
+                    task.delay(0.5, function()
+                        flash:Destroy()
+                    end)
+
+                    self._janitor:AddPromise(Promise.delay(self.Stats["FireRate"]):andThen(function()
+                        self.canFire = true
+                    end))
+                end
+            end
+        end))
+
         self._janitor:Add(function()
             ContextActionService:UnbindAction("Shoot")
+            self.canFire = false
+            self.isFiring = false
         end)
 
         self._janitor:Add(hum.Died:Connect(function()
